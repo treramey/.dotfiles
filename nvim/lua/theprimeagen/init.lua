4536d625e3b79666ad79772667a30a26d06c2b27
Trevor <optimus@pop-os.localdomain>
2022-09-08 22:37:04 -0500

    add nvim config

---
+++ nvim/lua/theprimeagen/init.lua
@@ -0,0 +1,41 @@
+require("theprimeagen.set")
+require("theprimeagen.packer")
+require("theprimeagen.neogit")
+require("theprimeagen.debugger")
+require("theprimeagen.text-me-daddy")
+
+local augroup = vim.api.nvim_create_augroup
+ThePrimeagenGroup = augroup('ThePrimeagen', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufEnter", "BufWinEnter", "TabEnter"}, {
+    group = ThePrimeagenGroup,
+    pattern = "*.rs",
+    callback = function()
+        require("lsp_extensions").inlay_hints{}
+    end
+})
+
+autocmd({"BufWritePre"}, {
+    group = ThePrimeagenGroup,
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
