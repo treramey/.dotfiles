6abf9467ae63c247760eacbc62d3c70b626cf4b7
Trevor <treramey@gmail.com>
2023-04-16 22:00:01 -0500

    add some new stuff

--- nvim/astronvim_config/plugins/treesitter.lua
+++ nvim/astronvim_config/plugins/treesitter.lua
@@ -3,19 +3,6 @@ return {
   opts = {
     ensure_installed = {
       "lua",
-      "python",
-      "bash",
-      "json",
-      "yaml",
-      "regex",
-      "comment",
-      "query",
-      "css",
-      "html",
-      "javascript",
-      "go",
-      "typescript",
-      "c_sharp",
     },
   },
 }
