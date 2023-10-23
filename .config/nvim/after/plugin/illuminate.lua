26ba1ef7943f2e2f14790615bef42700b82be9fc
Trevor <treramey@gmail.com>
2023-10-23 11:38:04 -0500

    add nvim

---
+++ .config/nvim/after/plugin/illuminate.lua
@@ -0,0 +1,13 @@
+require("illuminate").configure({
+	under_cursor = false,
+	filetypes_denylist = {
+		"DressingSelect",
+		"Outline",
+		"TelescopePrompt",
+		"alpha",
+		"harpoon",
+		"toggleterm",
+		"neo-tree",
+		"Spectre",
+	},
+})
---
+++ .config/nvim/after/plugin/indent_blankline.lua
@@ -0,0 +1,7 @@
+require("ibl").setup({
+	scope = {
+		enabled = false,
+		show_start = false,
+		show_end = false,
+	},
+})
