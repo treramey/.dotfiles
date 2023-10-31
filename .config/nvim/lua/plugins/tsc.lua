e59b329b7635eb985032403dc1dd9481307f6e04
Trevor <treramey@gmail.com>
2023-10-30 20:44:29 -0500

    update nvim

--- .config/nvim/lua/plugins/tsc.lua
+++ .config/nvim/lua/plugins/tsc.lua
@@ -1,12 +1,10 @@
 return {
-	{
-		dir = "~/Code/tsc-prs/pretty-errors/tsc.nvim",
-		branch = "pretty-ts-errors",
-		ft = { "typescript", "typescriptreact" },
-		config = function()
-			require("tsc").setup({
-				pretty_errors = true,
-			})
-		end,
-	},
+  {
+    'dmmulroy/tsc.nvim',
+    ft = { "typescript", "typescriptreact" },
+    config = function()
+      require("tsc").setup({
+      })
+    end,
+  },
 }
